92f9acd8aff60a06b49c24cf361dde9fc241d940
Drew W <drew@wks.us>
2023-07-04 15:19:37 -0400

    Update index.py

--- index.py
+++ index.py
@@ -7,10 +7,9 @@ def main():
 
     # get_completion is a helper function. It takes in a prompt and return a completion for that prompt
     def get_completion(prompt, model="gpt-3.5-turbo"):
-        messages = [{"role": "user", "content": prompt}]
         response = openai.ChatCompletion.create(
             model=model,
-            messages=messages,
+            messages=[{"role": "user", "content": prompt}],
             temperature=0,  # this is the degree of randomness of the model's output
         )
         return response.choices[0].message["content"]
