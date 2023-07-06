e6323cd709888a2ab4f3fdf2b11104d826b710cf
Drew W <drew@wks.us>
2023-07-06 18:35:44 -0400

    Update index.py

--- index.py
+++ index.py
@@ -33,8 +33,7 @@ def main():
              # Run your Python script with the submitted text
             response = get_completion(prompt)
            
-            st.spinner(text="This may take a moment..."):
-            
+            st.spinner(text="This may take a moment...")
             st.write(f"GPT 3.5 turbo Response: {response}")
             
 
