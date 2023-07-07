54f73e214b3f649cefecb3a3bc5da1ab4dc94238
Drew W <drew@wks.us>
2023-07-06 21:59:13 -0400

    Update index.py

--- index.py
+++ index.py
@@ -33,7 +33,7 @@ def main():
              # Run your Python script with the submitted text
             response = get_completion(prompt)
         
-            st.text_area(label ="Chat GPT response",value=response, height =100)
+            st.text_area(label ="Chat GPT response",value=response)
            # st.write(f"GPT 3.5 turbo Response: {response}")
             
 
