cd0fd969d8254469e29edadc8cbcab93f4c07126
Drew W <drew@wks.us>
2023-07-12 08:39:18 -0400

    Update index.py

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ def main():
     # get_completion is a helper function. It takes in a prompt and return a completion for that prompt
     def get_completion(prompt):
         response = openai.ChatCompletion.create(
-            model=model,
+            model=gpt-3.5-turbo,
             messages=[{"role": "user", "content": prompt}],
             temperature=0,  # this is the degree of randomness of the model's output
         )
@@ -19,7 +19,7 @@ def main():
     
     with st.form("my_form"):
         # Input widgets
-        model = st.selectbox("Select a model:", ("gpt-3.5-turbo", "gpt-4.0"))
+        # model = st.selectbox("Select a model:", ("gpt-3.5-turbo", "gpt-4.0")) # this works it's just not needed right now
         instruction = st.text_area("Place instructions here:")    
         text = st.text_area("Place optional text here:") 
         submit_button = st.form_submit_button("Submit")
@@ -34,8 +34,7 @@ def main():
              # Run your Python script with the submitted text
             response = get_completion(prompt)
         
-            # st.write(f"**Response:** {response}")
-            # st.write(f"**GPT Response:** {response}")
+            # st.write(f"**Response:** {response}") # this works. I just prefer the st.code model with copy 2 clipboard
             st.code(response, language="asciidoc", line_numbers=False)
             
 
