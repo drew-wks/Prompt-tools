66b7b02af05a385a6f26fccce0408013abd14bd2
Drew W <20048187+dvvilkins@users.noreply.github.com>
2023-07-26 18:29:17 -0400

    Update index.py

--- index.py
+++ index.py
@@ -54,22 +54,22 @@ def main():
             )
 #round two       
   
-    with st.form("round2"):
-        # Input widgets
-        instruction = st.text_area("Place instructions here:")    
-        submit_button2 = st.form_submit_button("Submit")
+        with st.form("round2"):
+            # Input widgets
+            instruction = st.text_area("Place instructions here:")    
+            submit_button2 = st.form_submit_button("Submit")
+            
         
-    
-    # When the submit button is clicked
-    if submit_button2:
-             # Run your Python script with the submitted text
-            
-            prompt = f"""
-                {instruction}```{response}```
-                """
-            
-            response = get_completion(prompt)
-            st.write(f"**Response:** {response}") 
+        # When the submit button is clicked
+        if submit_button2:
+                 # Run your Python script with the submitted text
+                
+                prompt = f"""
+                    {instruction}```{response}```
+                    """
+                
+                response = get_completion(prompt)
+                st.write(f"**Response:** {response}") 
             
 if __name__ == '__main__':
     main()
